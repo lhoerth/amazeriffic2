f098e02f4a4cc67fd6b3fde2a9bdc80b6705ad43
lhoerth <lhoerth@mail.greenriver.edu>
2015-01-22 08:20:59 -0800

    Added tab clicks in js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 var main = function () {
-"use strict";
-window.alert("hello world!");
+	"use strict";
+	
+	$(".tabs a span").toArray().forEach(function(element) {
+		$(element).on("click", function() {
+			var $element = $(element);
+			$(".tabs span").removeClass("active");
+			$element.addClass("active");
+			$("main .content").empty();
+			
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("FIRST TAB CLICKED!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("SECOND TAB CLICKED!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("THIRD TAB CLICKED!");
+			}
+			return false;
+		});
+	});
 };
-$(document).ready(main);
+
+main();
